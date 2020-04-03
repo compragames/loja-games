36667731c591bce35f36e772298b99bbaf2f80cc
gisidorio <>
2020-04-02 23:16:49 -0300

    Criação de GeralService e Model UserPost

---
+++ src/main/java/br/com/lojagames/Application/Services/GeralServices.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.lojagames.Application.Services;
+
+import br.com.lojagames.Application.Model.UserPost;
+
+/**
+ *
+ * @author gabri
+ */
+public class GeralServices {
+    
+    public UserPost userPost() {
+        return new UserPost();
+    }
+    
+    
+}
